e3356f5086a664710b5abbbe25835113f1beb19a
Bluefire <D33force@outlook.com>
2022-07-29 20:43:24 -0700

    Adding game launcher

---
+++ GameLauncher/GameLauncher/MainWindow.xaml.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Windows;
+using System.IO;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.ComponentModel;
+using System.Net;
+
+namespace GameLauncher
+{
+
+    enum LauncherStatus
+    {
+        ready,
+        failed,
+        downloadingGame,
+        downloadingUpdate
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private string rootPath;
+        private string versionFile;
+        private string gameZip;
+        private string gameExe;
+
+        private LauncherStatus _status;
+
+        internal LauncherStatus Status
+        {
+            get => _status;
+            set
+            {
+                _status = value;
+                switch (_status)
+                {
+                    case LauncherStatus.ready:
+                        PlayTekhael.Content = "Play";
+                        break;
+                    case LauncherStatus.failed:
+                        PlayTekhael.Content = "Update Failed - Retry";
+                        break;
+                    case LauncherStatus.downloadingGame:
+                        PlayTekhael.Content = "Downloading Game";
+                        break;
+                    case LauncherStatus.downloadingUpdate:
+                        PlayTekhael.Content = "Downloading Update";
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            rootPath = Directory.GetCurrentDirectory();
+            versionFile = Path.Combine(rootPath, "Version.txt");
+            gameZip = Path.Combine(rootPath, "Build.zip");
+            gameExe = Path.Combine(rootPath, "Build", "Tekhael.exe");
+
+        }
+
+        private void CheckforUpdates()
+        {
+            if (File.Exists(versionFile))
+            {
+                Version localVersion = new Version(File.ReadAllText(versionFile));
+                VersionText.Text = localVersion.ToString();
+
+                try
+                {
+                    WebClient webClient = new WebClient();
+                    Version onlineVersion = new Version(webClient.DownloadString("https://drive.google.com/uc?export=download&id=1RyqlUkN8vBuSmi8LLsLWX9uR1ri2HpUZ"));
+
+                    if (onlineVersion.IsDifferentThan(localVersion))
+                    {
+                        InstallGameFiles(true, onlineVersion);
+                    }
+                    else
+                    {
+                        Status = LauncherStatus.ready;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Status = LauncherStatus.failed;
+                    MessageBox.Show($"Error checking for game updates: {ex}");
+                }
+            }
+            else
+            {
+                InstallGameFiles(false, Version.zero);
+            }
+        }
+
+        private void InstallGameFiles(bool _isUpdate, Version _onlineVersion)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                if (_isUpdate)
+                {
+                    Status = LauncherStatus.downloadingUpdate;
+                }
+                else
+                {
+                    Status = LauncherStatus.downloadingGame;
+                    _onlineVersion = new Version(webClient.DownloadString("https://drive.google.com/uc?export=download&id=1RyqlUkN8vBuSmi8LLsLWX9uR1ri2HpUZ"));
+                }
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadGameCompletedCallback);
+                webClient.DownloadFileAsync(new Uri("https://drive.google.com/uc?export=download&id=16qFG22MaVi53zD6twAcZ_23MViRuItaE"), gameZip, _onlineVersion);
+            }
+            catch (Exception ex)
+            {
+                Status = LauncherStatus.failed;
+                MessageBox.Show($"Error installing game files: {ex}");
+            }
+        }
+
+        private void DownloadGameCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            try
+            {
+                string onlineVersion = ((Version)e.UserState).ToString();
+                ZipFile.ExtractToDirectory(gameZip, rootPath, true);
+                File.Delete(gameZip);
+
+                File.WriteAllText(versionFile, onlineVersion);
+
+                VersionText.Text = onlineVersion;
+                Status = LauncherStatus.ready;
+            }
+            catch (Exception ex)
+            {
+                Status = LauncherStatus.failed;
+                MessageBox.Show($"Error finishing download: {ex}");
+            }
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            CheckforUpdates();
+        }
+
+        private void PlayTekhael_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(gameExe) && Status == LauncherStatus.ready)
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo(gameExe);
+                startInfo.WorkingDirectory = Path.Combine(rootPath, "Build");
+                Process.Start(startInfo);
+
+                Close();
+            }
+            else if (Status == LauncherStatus.failed)
+            {
+                CheckforUpdates();
+            }
+        }
+    }
+
+    struct Version
+    {
+        internal static Version zero = new Version(0, 0, 0);
+
+        private short major;
+        private short minor;
+        private short subMinor;
+
+        internal Version(short _major, short _minor, short _subMinor)
+        {
+            major = _major;
+            minor = _minor;
+            subMinor = _subMinor;
+        }
+
+        internal Version(string _version)
+        {
+            string[] _versionStrings = _version.Split('.');
+
+            if (_versionStrings.Length != 3)
+            {
+                major = 0;
+                minor = 0;
+                subMinor = 0;
+                return;
+            }
+
+            major = short.Parse(_versionStrings[0]);
+            minor = short.Parse(_versionStrings[1]);
+            subMinor = short.Parse(_versionStrings[2]);
+
+        }
+
+        internal bool IsDifferentThan(Version _otherVersion)
+        {
+            if (major != _otherVersion.major)
+            {
+                return true;
+            }
+            else
+            {
+                if (minor != _otherVersion.minor)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (subMinor != _otherVersion.subMinor)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"{major}.{minor}.{subMinor}";
+        }
+
+    }
+
+}
